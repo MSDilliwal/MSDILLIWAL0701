88b99cc9dac701b97a6c8fec0d65e659598e6b2d
Hp Lap <Hp Lap@DESKTOP-B4CSBRG>
2022-07-20 17:59:40 +0530

    ewr

---
+++ org.tyss.project/src/test/java/testNg_Examples_Test/Example1_Test.java
@@ -0,0 +1,34 @@
+package testNg_Examples_Test;
+
+import org.testng.annotations.Test;
+
+public class Example1_Test {
+
+	@Test
+	public void showow()
+	{
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void run()
+	{
+		System.out.println("Hii");
+		System.out.println("Good morning");
+	}
+	
+	@Test	public void display()
+	{
+		System.out.println("Hey");
+		String url =	System.getProperty("url");
+		String browser =System.getProperty("browser");
+		System.out.println("url ="+ url);
+		System.out.println("browser ="+browser);
+		System.out.println("Hi everyone ");
+		
+		System.out.println("poll scm");
+		System.out.println("commit aagoo");
+				
+	}
+	
+}
---
+++ org.tyss.project/src/test/java/testNg_Examples_Test/Example2_Test.java
@@ -0,0 +1,27 @@
+package testNg_Examples_Test;
+
+import org.testng.annotations.Test;
+
+public class Example2_Test {
+
+@Test
+public void chat()
+{
+  System.out.println("chat method");
+  System.out.println("chat method is been dislplayed");
+}
+
+@Test
+public void status()
+{
+  System.out.println("Status method");	
+}
+
+@Test
+public void call()
+{
+  System.out.println("call method");	
+}
+
+}
+
